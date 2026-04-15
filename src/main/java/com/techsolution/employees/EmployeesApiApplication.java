a69c3548bddb20c85dff5c2e5a82a7dcc55fff70
Zelko7u7 <99aguirreandres01@gmail.com>
2026-04-15 07:55:07 -0500

    Point 1: initialize Spring Boot project and database configuration

---
+++ src/main/java/com/techsolution/employees/EmployeesApiApplication.java
@@ -0,0 +1,13 @@
+package com.techsolution.employees;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeesApiApplication.class, args);
+	}
+
+}
